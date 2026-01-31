96ad9bcff8ce939ffe7294249edaf6345a1cc95c
Gonor13 <petrenkonikita13@yandex.ru>
2026-02-01 01:53:09 +0300

    feat: initial working version of Base Level app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@
 
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { useAccount } from 'wagmi'
-import LevelCard from '@/components/LevelCard'
+import LevelCard from '../components/LevelCard'
 
 export default function Page() {
   const { address, isConnected } = useAccount()
 
   return (
-    <main className='min-h-screen flex flex-col items-center justify-center bg-black text-white px-4'>
-      <h1 className='text-3xl font-bold mb-6'>Base Level</h1>
+    <main className="min-h-screen flex flex-col items-center justify-center bg-black text-white px-4">
+      <h1 className="text-3xl font-bold mb-6">Base Level</h1>
       <ConnectButton />
       {isConnected && address && <LevelCard address={address} />}
     </main>
